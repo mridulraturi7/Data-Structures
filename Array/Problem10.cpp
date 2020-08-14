24a9c9356002bb9696d6ee7ba648632fc5a09319
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-14 23:19:05 +0530

    [Modify] Array/Problem10.cpp

--- Array/Problem10.cpp
+++ Array/Problem10.cpp
@@ -1,5 +1,7 @@
 /*
     At least two greater elements.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/at-least-two-greater-elements/0
 */
 
 #include<iostream>
