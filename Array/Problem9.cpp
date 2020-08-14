ea3f1949c8d5b849d0b71283a2b25785a9d8cc9c
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-14 20:49:26 +0530

    [Done Problem9] Array/Problem9.cpp

---
+++ Array/Problem9.cpp
@@ -0,0 +1,50 @@
+/*
+    Pallindromic Array
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int checkPallindromeArray(int[], int n);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, array[20];
+        cin>>n;
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        cout<<checkPallindromeArray(array, n)<<endl;
+    }
+
+    return 0;
+}
+
+int checkPallindromeArray(int array[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        int remainder, reverseNum = 0, originalNum = array[i];
+        while(array[i] != 0)
+        {
+            remainder = array[i] % 10;
+            reverseNum = reverseNum * 10 + remainder;
+            array[i] /= 10;
+        }
+
+        if(originalNum != reverseNum)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
