179930b0c4cec260a25ddda81ed5d6d764029fa7
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-15 17:55:56 +0530

    [Done Problem14] Array/Problem14.cpp

---
+++ Array/Problem14.cpp
@@ -0,0 +1,36 @@
+/*
+    Check if an array is sorted?
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while (testCase-- != 0)
+    {
+        int n, array[500], flag = 1;
+        cin>>n;
+        
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        for(int i = 0 ; i < n - 1; i++)
+        {
+            if(array[i] < array[i+1])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        cout<<flag<<endl;
+    }
+    
+    return 0;
+}
