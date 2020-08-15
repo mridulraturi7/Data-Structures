27ffee8a24cdbee6b38dee9243c31777cbb471fc
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-16 00:43:04 +0530

    [Done Problem20] Array/Problem20.cpp

---
+++ Array/Problem20.cpp
@@ -0,0 +1,38 @@
+/*
+    C++ 2D Arrays - Find the transpose of a 2D Matrix.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, array[100][100];
+        cin>>n;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                cin>>array[i][j];
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                cout<<array[j][i]<< " ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
