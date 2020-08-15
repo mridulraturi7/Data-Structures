e4376d344d37fbc6ce34a77072913e25c077781b
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-15 19:02:52 +0530

    [Modify] Array/Problem16.cpp

--- Array/Problem16.cpp
+++ Array/Problem16.cpp
@@ -1,5 +1,7 @@
 /*
     Print the left element.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/print-the-left-element/0#ExpectOP
 */
 
 #include<iostream>
