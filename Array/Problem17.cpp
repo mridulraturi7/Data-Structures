7eb9df897011e2c27b4a2928958b897429aa320c
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-15 21:15:55 +0530

    [Done Problem17] Array/Problem17.cpp

---
+++ Array/Problem17.cpp
@@ -0,0 +1,33 @@
+/*
+    Average
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, array[50], sum = 0;
+        cin>>n;
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            sum = sum + array[i];
+            cout<<sum/(i+1)<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
