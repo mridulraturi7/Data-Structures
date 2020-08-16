a27d7f89100751ce43f4c35f7c65dcb901a3e534
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-16 20:55:32 +0530

    [Modify] Array/Problem24.cpp

--- Array/Problem24.cpp
+++ Array/Problem24.cpp
@@ -1,5 +1,7 @@
 /*
     Value equal to index value.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/value-equal-to-index-value/0
 */
 
 #include<iostream>
