588f7d976a562505c7b657fb6edd0268993d7c70
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-16 20:44:46 +0530

    [Done Problem23] Array/Problem23.cpp

---
+++ Array/Problem23.cpp
@@ -0,0 +1,39 @@
+/*
+    Smaller and Larger.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, x, array[100], smaller = 0, larger = 0;
+        cin>>n>>x;
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(array[i] <= x)
+            {
+                smaller++;
+            }
+
+            if(array[i] >= x)
+            {
+                larger++;
+            }
+        }
+        cout<<smaller<<" "<<larger<<endl;
+    }
+    return 0;
+}
