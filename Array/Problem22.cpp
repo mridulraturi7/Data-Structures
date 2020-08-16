3989778bbd78787cb95c37e316247bf3f425f993
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-16 20:35:50 +0530

    [Done Problem22] Array/Problem22.cpp

---
+++ Array/Problem22.cpp
@@ -0,0 +1,47 @@
+/*
+    Find Index.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, array[100001], key;
+        cin>>n;
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        cin>>key;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(array[i] == key)
+            {
+                cout<<i<<" ";
+                break;
+            }
+        }
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(array[i] == key)
+            {
+                cout<<i;
+                break;
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
