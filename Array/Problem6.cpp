4fc092daf5748c9f16349a4c5d9c36f9e065eb9a
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-17 20:58:26 +0530

    [ADD Info] Array/Problem6.cpp

--- Array/Problem6.cpp
+++ Array/Problem6.cpp
@@ -2,6 +2,7 @@
     Sum of Series.
     Problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/sum-of-series/0
+    Difficulty - Basic
 */
 
 #include<iostream>
