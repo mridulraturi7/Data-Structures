a73c8e882989adf8dc2b98f6af0d8b203c8900fc
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-19 23:38:34 +0530

    [Working] Array/Problem28.cpp

--- Array/Problem28.cpp
+++ Array/Problem28.cpp
@@ -2,3 +2,36 @@
     Rotate an array.
 */
 
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, d, input[10000001], temp[10000001];
+        cin>>n>>d;
+
+        for(int i = 0 ; i < n; i++)
+        {
+            cin>>input[i];
+        }
+
+        for(int i = 0; i < d; i++)
+        {
+            temp[i] = input[i];
+        }
+
+        for(int i = 0; i < n - d; i++)
+        {
+            input[i] = input[i + d];
+        }
+    }
+
+    return 0;
+}
+
