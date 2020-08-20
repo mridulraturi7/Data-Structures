d4a12687293ba305a3a07c7a56a7faf3e882011b
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-20 11:48:28 +0530

    [Done Problem 31] Array/Problem31.cpp

---
+++ Array/Problem31.cpp
@@ -0,0 +1,61 @@
+/*
+    Maximum in struct array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+struct Height
+{
+    int feet;
+    int inches;
+};
+
+int findMax(Height arr[], int n);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, fe, in;
+        cin>>n;
+
+        Height arr[100000];
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>fe>>in;
+            arr[i] = {fe, in};
+        }
+
+        int res = findMax(arr, n);
+        cout<<res<<endl;
+    }
+
+    return 0;
+}
+
+int findMax(Height arr[], int n)
+{
+    int height[100000];
+    for(int i = 0; i < n; i++)
+    {
+        height[i] = ((arr[i].feet * 12) + arr[i].inches);
+    }
+
+    int maxHeight = height[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(height[i] > maxHeight)
+        {
+            maxHeight = height[i];
+        }
+    }
+
+    return maxHeight;
+}
