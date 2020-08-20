9437a0862f484c202b52a44ce12fc0d5bbc39b37
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-20 18:13:08 +0530

    [Done Problem 34] Array/Problem34.cpp

---
+++ Array/Problem34.cpp
@@ -0,0 +1,43 @@
+/*
+    Sherlock a detective
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while (testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int v1[100000];
+        int v2[100000] = { 0 };
+        for(int i = 0; i < n; i++)
+        {
+            cin>>v1[i];
+        }
+
+        for(int i = 1; i < n; i++)
+        {
+            v2[v1[i] - 1] = 1;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(v2[i] == 0)
+            {
+                cout<<i + 1<<" ";
+            }
+        }
+
+        cout<<endl;
+    }
+    
+    return 0;
+}
