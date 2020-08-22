8d2be538b283b2b1102d9c2608e3428ad93d66d6
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-22 23:25:37 +0530

    [Modify] Array/Problem3.cpp

--- Array/Problem3.cpp
+++ Array/Problem3.cpp
@@ -2,7 +2,7 @@
     Sum of Array Elements.
     Problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/sum-of-array-elements/0
-    Difficulty - Basic
+    Difficulty - School
 */
 
 #include<iostream>
