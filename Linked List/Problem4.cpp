7c393a37ead3b49914e7bcf799b7e70029977fb4
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-23 11:13:16 +0530

    [Done Problem 4] LinkedList/Problem4.cpp

---
+++ Linked List/Problem4.cpp
@@ -0,0 +1,91 @@
+/*
+    Deleting a node at a given position. Take Head Position = 0.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+};
+
+void printList(Node *n)
+{
+    while(n != NULL)
+    {
+        cout<<n->data<<" ";
+        n = n->next;
+    }
+}
+
+void deleteNode(Node **head_ref, int position)
+{
+    Node *temp = *head_ref, *previous_node;
+
+    //Check if the node to be deleted is the head node
+    if(temp != NULL && position == 0)
+    {
+        *head_ref = temp->next;
+        delete temp;
+        return;
+    }
+
+    int count = 0;
+
+    //Else find te previous node of the node to be deleted.
+    while(temp != NULL)
+    {
+        previous_node = temp;
+        temp = temp->next;
+
+        count++;
+
+        if(count == position)
+        {
+            previous_node->next = temp->next;
+            break;
+        }
+    }
+
+    //Free the memory allocated to the node to be deleted.
+    delete temp;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node();
+    second  = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    cout<<"Current Status of the Linked List is: ";
+    printList(head);
+
+    int position;
+
+    cout<<"Enter the position of the node to be deleted: ";
+    cin>>position;
+
+    deleteNode(&head, position);
+
+    cout<<"Status of Linked List after deleting node at "<<position<<" position: ";
+    printList(head);
+
+    return 0;
+}
