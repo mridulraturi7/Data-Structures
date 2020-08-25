a4cbe2f1ac8b0a17a6877b294b9cacf01522eabc
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-25 23:02:18 +0530

    [Modify] Array/Problem40.cpp

--- Array/Problem40.cpp
+++ Array/Problem40.cpp
@@ -1,5 +1,8 @@
 /*
     Find the fine.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/find-the-fine/0
+    Difficulty - Basic
 */
 
 #include<iostream>
