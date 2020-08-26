c41a239929bf807baa17caa36c0370ec9bae75b2
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-26 23:29:13 +0530

    [Done Problem 8] LinkedList/Problem8.cpp

---
+++ Linked List/Problem8.cpp
@@ -0,0 +1,77 @@
+/*
+    Searching an element in Linked List
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Node
+{
+    public: 
+        int data;
+        Node *next;
+};
+
+bool searchElement(Node *n, int value)
+{
+    while(n != NULL)
+    {
+        if(n->data == value)
+        {
+            return true;
+        }
+        n = n->next;
+    }
+
+    return false;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+    Node *fourth = NULL;
+    Node *fifth = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+    fourth = new Node();
+    fifth = new Node();
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 0;
+    third->next = fourth;
+
+    fourth->data = 50;
+    fourth->next = fifth;
+
+    fifth->data = 40;
+    fifth->next = NULL;
+
+    int key;
+
+    cout<<"Enter the element to search in the linked list: ";
+    cin>>key;
+
+    bool res = searchElement(head, key);
+
+    if(res == true)
+    {
+        cout<<"Element is found";
+    }
+
+    else
+    {
+        cout<<"Element is not found";
+    }
+
+    return 0;
+}
