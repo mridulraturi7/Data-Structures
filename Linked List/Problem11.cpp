d2feef6ec326a8ab18fb42c5a0c111de496112e5
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-27 23:15:17 +0530

    [Modify] LinkedList/Problem11.cpp

--- Linked List/Problem11.cpp
+++ Linked List/Problem11.cpp
@@ -1,5 +1,7 @@
 /*
     Get nth node from Linked List (Recursive Approach).
+    You are given a linked list, you have to write a function that returns the nth node from the linked list.
+    Assume indexing starts from 0.
 */
 
 #include<iostream>
