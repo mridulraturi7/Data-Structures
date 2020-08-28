a20eb3dd9307f04a7dfa8b63193a0889cd2240f8
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-28 20:15:36 +0530

    [Done Problem 44] Array/problem44.cpp

---
+++ Array/Problem44.cpp
@@ -0,0 +1,54 @@
+/*
+    Positive and Negative Elements.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+	cin>>testCase;
+	
+	while(testCase-- != 0)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int *array = new int[n];
+	    int *positive = new int[n/2];
+	    int *negative = new int[n/2];
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+	        cin>>array[i];
+	    }
+	    
+	    
+	    int po = 0, ne = 0;
+	    for(int i = 0; i < n; i++)
+	    {
+	        if(array[i] >= 0)
+	        {
+	            positive[po] = array[i];
+	            po++;
+	        }
+	        
+	        else if(array[i] < 0)
+	        {
+	            negative[ne] = array[i];
+	            ne++;
+	        }
+	    }
+	    
+	    for(int i = 0; i < n/2; i++)
+	    {
+	        cout<<positive[i]<<" "<<negative[i]<<" ";
+	    }
+	    
+	    cout<<endl;
+	}
+	
+	return 0;
+}
