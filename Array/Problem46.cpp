444884b29fd1ab8df9cdeca94de007a6213c4557
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-28 23:42:50 +0530

    [Done Problem 46] Array/Problem46.cpp

---
+++ Array/Problem46.cpp
@@ -0,0 +1,71 @@
+/*
+    Find triplets with zero sum.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+bool checkTripletsSum(int *array, int n)
+{
+    sort(array, array + n);
+
+    for(int i = 0; i < n; i++)
+    {
+        int l = i + 1, r = n - 1;
+
+        while(l < r)
+        {
+            int sum = array[i] + array[l] + array[r];
+            if(sum < 0)
+            {
+                l++;
+            }
+
+            else if(sum > 0)
+            {
+                r--;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        bool res = checkTripletsSum(array, n);
+
+        if(res)
+        {
+            cout<<"1"<<endl;
+        }
+
+        else
+        {
+            cout<<"0"<<endl;
+        }
+        
+    }
+    return 0;
+}
