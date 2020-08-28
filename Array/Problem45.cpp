5d4761ea465ae90b9c15b8ff3dcff2967781d763
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-28 20:44:44 +0530

    [Done Problem 45] Array/Problem45.cpp

---
+++ Array/Problem45.cpp
@@ -0,0 +1,68 @@
+/*
+    Pendulum Arrangement of an Array.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        int *output = new int[n];
+ 
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        sort(array, array + n);
+
+        int mid;
+
+        if((n % 2) == 0)
+        {
+            mid = (n - 1)/2;
+        }
+
+        else
+        {
+            mid = n/2;
+        }
+
+        output[mid] = array[0];
+
+        int right = mid, left = mid;
+
+        for(int i = 1; i < n; i++)
+        {
+            if((i%2) != 0)
+            {
+                output[++right] = array[i]; 
+            }
+
+            else
+            {
+                output[--left] = array[i];
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cout<<output[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
