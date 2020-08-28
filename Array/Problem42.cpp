8a2706c329d3d7faa43bd4c482a7ab2508e05a54
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-28 13:21:12 +0530

    [Done Problem 42] Array/Problem42.cpp

---
+++ Array/Problem42.cpp
@@ -0,0 +1,55 @@
+/*
+    Maximum Product of two numbers.
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        long n;
+        cin>>n;
+
+        vector<int> v(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>v[i];
+        }
+
+        int max1 = 0, max2 = 0, max_index, prod = 1;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(v[i] > max1)
+            {
+                max1 = v[i];
+                max_index = i;
+            }
+        }
+
+        v[max_index] = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(v[i] > max2)
+            {
+                max2 = v[i];
+            }
+        }
+
+        prod = max1 * max2;
+
+        cout<<prod<<endl;
+
+    }
+
+    return 0;
+}
