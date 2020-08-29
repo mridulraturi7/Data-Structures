d506cd9bc94811e85fd410cf60808daa8faac1da
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-29 21:30:31 +0530

    [Done Problem 52] Array/Problem52.cpp

---
+++ Array/Problem52.cpp
@@ -0,0 +1,60 @@
+/*
+    Removing duplicate elements from sorted array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int removeDuplicateElements(int array[], int n);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int array[7]; //= new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int size = removeDuplicateElements(array, n);
+
+        for(int i = 0; i < size; i++)
+        {
+            cout<<array[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+int removeDuplicateElements(int array[], int n)
+{
+    int *temp = new int[n];
+    int j = 0;
+    for(int i = 0; i < n - 1; i++)
+    {
+        if(array[i] != array[i + 1])
+        {
+            temp[j++] = array[i];
+        }
+    }
+
+    temp[j++] = array[n - 1];
+
+    for(int i = 0; i < j; i++)
+    {
+        array[i] = temp[i];
+    } 
+    
+    return j;
+}
