1aa220d5ea9116d02cd7a442c13810101bb63b4e
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-31 20:30:59 +0530

    [Done Problem 57] Array/Problem57.cpp

---
+++ Array/Problem57.cpp
@@ -0,0 +1,57 @@
+/*
+    Product of maximum in first array and minimum in second array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n1;
+        cin>>n1;
+
+        long long *array1 = new long long[n1];
+        for(int i = 0; i < n1; i++)
+        {
+            cin>>array1[i];
+        }
+
+        int n2;
+        cin>>n2;
+
+        long long *array2 = new long long[n2];
+        for(int i = 0; i < n2; i++)
+        {
+            cin>>array2[i];
+        }
+
+        long long prod, max = -1000000000, min = 100000000;
+        for(int i = 0; i < n1; i++)
+        {
+            if(array1[i] > max)
+            {
+                max = array1[i];
+            }
+        } 
+
+        for(int i = 0; i < n2; i++)
+        {
+            if(array2[i] < min)
+            {
+                min = array2[i];
+            }
+        }
+
+        prod = max * min;
+
+        cout<<prod<<endl;
+    }
+
+    return 0;
+}
