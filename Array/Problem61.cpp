724b560fffafe71809d01472250c2db6b6b7af5c
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-01 09:21:15 +0530

    [Done Problem 61] Array/Problem61.cpp

---
+++ Array/Problem61.cpp
@@ -0,0 +1,53 @@
+/*
+    Rotating an array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, d;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        cin>>d;
+
+        int *temp = new int[d];
+        for(int i = 0; i < d; i++)
+        {
+            temp[i] = array[i];
+        }
+
+        for(int i = 0; i < n - d; i++)
+        {
+            array[i] = array[i + d];
+        }
+
+        int j = 0;
+        for(int i = n - d; i < n; i++)
+        {
+            array[i] = temp[j++];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cout<<array[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
