2be97e20d280785eb5a5575bf76e5588e6884f07
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-01 23:39:21 +0530

    [Done Problem 66] Array/Problem66.cpp

---
+++ Array/Problem66.cpp
@@ -0,0 +1,51 @@
+/*
+    Majority Element.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int max = *max_element(array, array + n);
+
+        int *hash = new int[max + 1] ();
+
+        for(int i = 0; i < n; i++)
+        {
+            hash[array[i]]++;
+        }
+
+        int majority_element = -1;
+
+        for(int i = 0; i <= max; i++)
+        {
+            if(hash[i] > n/2)
+            {
+                majority_element = i;
+                break;
+            }
+        }
+
+        cout<<majority_element<<endl;
+        
+    }
+
+    return 0;
+}
