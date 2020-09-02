9615f0b87b181c0215df813875b45ea3e4fdfa9b
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-02 21:15:23 +0530

    [Done Problem 69] Array/Problem69.cpp

---
+++ Array/Problem69.cpp
@@ -0,0 +1,57 @@
+/*
+    Total Count.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int countPerElement(int, int);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, k;
+        cin>>n>>k;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int total_element_count = 0;
+
+        for(int i= 0; i < n; i++)
+        {
+            total_element_count += countPerElement(array[i], k);
+        }
+
+        cout<<total_element_count<<endl;
+    }
+
+    return 0;
+}
+
+int countPerElement(int num, int k)
+{
+    int rem = (num % k);
+    int elements = 0;
+    
+    if(rem == 0)
+    {
+        elements = (num / k);
+    }
+    
+    else
+    {
+        elements = (num / k) + 1;
+    }
+    
+
+    return elements;  
+}
