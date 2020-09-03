7ae2c1042107e662d6080a68bd30c8a684f29b7b
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-03 20:45:34 +0530

    [Done problem 72] Array/Problem72.cpp

---
+++ Array/Problem72.cpp
@@ -0,0 +1,56 @@
+/*
+    Minimum Distance between two numbers.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int minDistance(int array[], int n, int x, int y);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int x, y;
+        cin>>x>>y;
+
+        cout<<minDistance(array, n, x, y)<<endl;
+    }
+}
+
+int minDistance(int array[], int n, int x, int y)
+{
+    int minDistance = n, prev_index = -1;
+    for(int i = 0; i < n; i++)
+    {
+        if(array[i] == x || array[i] == y)
+        {
+            if (prev_index != -1 && array[i] != array[prev_index])
+            {
+                minDistance = min(minDistance, i - prev_index);
+            }
+            prev_index = i;
+        }
+    }
+
+    if(minDistance == n)
+    {
+        return -1;
+    }
+
+    return minDistance;
+}
