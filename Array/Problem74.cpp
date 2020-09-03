93947cb15a47fe66caf32bdc284e1bae763e167c
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-03 23:51:17 +0530

    [Working] Array/Problem74.cpp

---
+++ Array/Problem74.cpp
@@ -0,0 +1,27 @@
+/*
+    Count Possible Triangles.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+    }
+
+    return 0;
+}
