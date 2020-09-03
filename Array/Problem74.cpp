cf18d28783b7f5e4b5be95d27664c659ac67091d
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-03 23:52:59 +0530

    [Working] Array/Problem74.cpp

--- Array/Problem74.cpp
+++ Array/Problem74.cpp
@@ -21,6 +21,21 @@ int main()
         {
             cin>>array[i];
         }
+
+        int count = 0;
+
+        for(int i = 0; i < n - 2; i++)
+        {
+            for(int j = i + 1; j < n - 1; j++)
+            {
+                for(int k = j + 1; k < n; k++)
+                {
+                    count++;
+                }
+            }
+        }
+
+        cout<<count<<endl;
     }
 
     return 0;
