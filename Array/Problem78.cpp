db77a1434fda1c834c9b38036c71febdb0b165f6
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-04 20:47:04 +0530

    [Done Problem 78] Array/Problem78.cpp

---
+++ Array/Problem78.cpp
@@ -0,0 +1,48 @@
+/*
+    Maximum Repeating Number.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, k;
+        cin>>n>>k;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int *hash = new int[k] ();
+        for(int i = 0; i < n; i++)
+        {
+            ++hash[array[i]];
+        }
+
+        int most_frequent = *max_element(hash, hash + k);
+        //int least_element = k - 1;
+        
+        for(int i = 0; i < k; i++)
+        {
+            if(hash[i] == most_frequent)
+            {
+                cout<<i;
+                break;
+            }
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
