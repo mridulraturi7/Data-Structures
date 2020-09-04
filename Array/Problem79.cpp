49bf27843448ca9f7d55347d3d11b699d1a0c515
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-04 21:01:57 +0530

    [Done Problem 79] Array/Problem79.cpp

---
+++ Array/Problem79.cpp
@@ -0,0 +1,52 @@
+/*
+    Count the numbers.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+bool checkNumber(int num);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, count = 0;
+        cin>>n;
+
+        for(int i = 1; i <= n; i++)
+        {
+            bool check = checkNumber(i);
+
+            if(check == true)
+            {
+                ++count;
+            }
+        }
+
+        cout<<count<<endl;
+    }
+
+    return 0;
+}
+
+bool checkNumber(int num)
+{
+    while(num != 0)
+    {
+        int digit = num % 10;
+
+        if(digit != 1 && digit != 2 && digit != 3 && digit != 4 && digit != 5)
+        {
+            return false;
+        }
+
+        num = num / 10;
+    }
+
+    return true;
+}
