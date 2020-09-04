ba827e5434f498fe8e4406b4377ae47680aadc85
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-04 23:50:43 +0530

    [Done Problem 81] Array/Problem81.cpp

---
+++ Array/Problem81.cpp
@@ -0,0 +1,53 @@
+/*
+    Count number of elements between two given elements in array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int num1, num2;
+        cin>>num1>>num2;
+
+        int l = 0, r = n - 1, l_index, r_index;
+        while(l < r)
+        {
+            if(array[l] != num1)
+            {
+                l++;
+            }
+        
+            if(array[r] != num2)
+            {
+                r--;
+            }
+
+            if(array[l] == num1 && array[r] == num2)
+            {
+                l_index = l;
+                r_index = r;
+                break;
+            }
+        }
+
+        cout<<((r_index - l_index) - 1)<<endl;
+    }
+
+    return 0;
+}
