7392b5c4c00170667a72c074d597a8472dbc1af9
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-04 12:07:41 +0530

    [Done Problem 77] Array/Problem77.cpp

---
+++ Array/Problem77.cpp
@@ -0,0 +1,61 @@
+/*
+    Pythagorean Triplet.
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+string pythagoreanTriplet(int array[], int n)
+{
+    sort(array, array + n);
+
+    for(int i = n - 1; i > 1; i--)
+    {
+        int rhs = array[i] * array[i];
+        int l = 0, r = i - 1;
+        while(l < r)
+        {
+            int lhs = (array[l] * array[l]) + (array[r] * array[r]);
+            if(lhs == rhs)
+            {
+                return "Yes";
+            }
+
+            else if(lhs < rhs)
+            {
+                l++;
+            }
+
+            else
+            {
+                r--;
+            }
+            
+        }
+    }
+    
+    return "No";
+}
+
+int main() {
+	int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        cout<<pythagoreanTriplet(array, n)<<endl;
+    }
+	return 0;
+}
