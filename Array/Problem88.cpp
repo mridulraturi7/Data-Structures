0968e9f89f8d7283879cf657ab50425a197a46e2
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-05 18:34:57 +0530

    [Modify] Array/Problem88.cpp

--- Array/Problem88.cpp
+++ Array/Problem88.cpp
@@ -1,5 +1,8 @@
 /*
     Type of Array.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/type-of-array/0
+    Difficulty - Basic
 */
 
 #include<iostream>
