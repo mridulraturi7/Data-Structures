90a597e255b0acfe6811f1a1210ee089018bcd05
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-06 21:17:15 +0530

    [Done Problem 95] Array/Problem95.cpp

---
+++ Array/Problem95.cpp
@@ -0,0 +1,47 @@
+/*
+    Reverse array in groups.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, k;
+        cin>>n>>k;
+
+        long *array = new long[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        for(int i = 0; i < n; i += k)
+        {
+            int l = i;
+            int r = (i + k < n) ? i + (k - 1) : n - 1;
+            while(l < r)
+            {
+                swap(array[l], array[r]);
+                l++;
+                r--;
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cout<<array[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
