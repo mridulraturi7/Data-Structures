4c5f8200c1a7cfb4f20b8fdf9ae9da1abecdaebc
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-06 23:24:57 +0530

    [Modify] Array/Problem95.cpp

--- Array/Problem95.cpp
+++ Array/Problem95.cpp
@@ -1,7 +1,7 @@
 /*
     Reverse array in groups.
     Problem is taken from GFG.
-    Problem Statement - https://practice.geeksforgeeks.org/problems/reverse-array-in-groups/0
+    Problem Statement - https://practice.geeksforgeeks.org/problems/shortest-un-ordered-subarray/0
     Difficulty - Basic
 */
 
