22f8e4249ffb27f1ba396ff0532dfdb6c3f482f9
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-08 00:01:35 +0530

    [Done Problem 101] Array/Problem101.cpp

---
+++ Array/Problem101.cpp
@@ -0,0 +1,59 @@
+/*
+    Sort in specific order.
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        vector<int> vec(n);
+        for(int i = 0; i < n; i++)
+        {
+            cin>>vec[i];
+        }
+
+        vector<int> even, odd;
+        for(int i = 0; i < n; i++)
+        {
+            if(vec[i] % 2 == 0)
+            {
+                even.push_back(vec[i]);
+            }
+
+            else
+            {
+                odd.push_back(vec[i]);
+            }
+            
+        }
+
+        sort(odd.begin(), odd.end(), greater<int>());
+        sort(even.begin(), even.end());
+
+        for(int i = 0; i < odd.size(); i++)
+        {
+            cout<<odd[i]<<" ";
+        }
+
+        for(int i = 0; i < even.size(); i++)
+        {
+            cout<<even[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
