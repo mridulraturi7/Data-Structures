8c714c8961ca9cdcfc881aaf7423241a4768a2c0
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-07 21:09:12 +0530

    [Done Problem] Array/Problem97.cpp

---
+++ Array/Problem97.cpp
@@ -0,0 +1,59 @@
+/*
+    Sorting Employees.
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+class Employee
+{
+    public:
+        string name;
+        int salary;
+};
+
+bool compareEmployee(Employee a, Employee b)
+{
+    if(a.salary < b.salary)
+        return true;
+
+    else if(a.salary == b.salary)
+    {
+        return a.name < b.name;
+    }
+
+    return false;
+    
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        Employee *array = new Employee[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i].name>>array[i].salary;
+        }
+
+        sort(array, array + n, compareEmployee);
+
+        for(int i = 0; i < n; i++)
+        {
+            cout<<array[i].name<<" "<<array[i].salary<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0; 
+}
