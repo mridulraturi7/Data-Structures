b0a510b40eae687de74c090e87f0705d75327fb0
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-07 23:20:45 +0530

    [Done Problem 99] Array/Problem99.cpp

---
+++ Array/Problem99.cpp
@@ -0,0 +1,36 @@
+/*
+    Maximum Value after m range operation.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, m;
+        cin>>n>>m;
+
+        int *array = new int[n] ();
+
+        for(int i = 0; i < m; i++)
+        {
+            int a, b, k;
+            cin>>a>>b>>k;
+            for(int j = a; j <= b; j++)
+            {
+                array[j] += k;
+            }
+        }
+
+        cout<<*max_element(array, array + n)<<endl;
+    }
+
+    return 0;
+}
