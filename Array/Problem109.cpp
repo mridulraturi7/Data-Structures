46b44860238a2228368de4884decb1769390d4df
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-08 21:10:26 +0530

    [Done Problem 109] Array/Problem109.cpp

---
+++ Array/Problem109.cpp
@@ -0,0 +1,41 @@
+/*
+    Count Pairs Odd XOR.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int count = 0;
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if((array[i] ^ array[j]) % 2 != 0)
+                {
+                    count++;
+                } 
+            }
+        }
+
+        cout<<count<<endl;
+    }
+
+    return 0;
+}
