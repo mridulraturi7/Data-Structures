837dfdd59c3e0bcaf281a75e0e9ece9d78017cc4
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-08 08:45:14 +0530

    [Done Problem 103] Array/Problem103.cpp

---
+++ Array/Problem103.cpp
@@ -0,0 +1,42 @@
+/*
+    Sum of f(a[i], a[j]) over all pairs in an array of n integers.
+*/
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int sum = 0;
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(abs(array[j] - array[i]) > 1)
+                {
+                    sum += (array[j] - array[i]);
+                }
+            }
+        }
+
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
