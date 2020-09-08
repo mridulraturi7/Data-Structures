49f057b121ff6ac12a7a45477842d1641f5cdc8d
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-08 23:28:21 +0530

    [Done Problem 110] Array/Problem110.cpp

---
+++ Array/Problem110.cpp
@@ -0,0 +1,52 @@
+/*
+    First Come First Serve.
+*/
+
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, k;
+        cin>>n>>k;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        unordered_map<int, int> hash;
+
+        for(int i = 0; i < n; i++)
+        {
+            hash[array[i]]++;
+        }
+
+        int found = -1;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(hash[array[i]] == k)
+            {
+                found = 1;
+                cout<<array[i]<<endl;
+                break;
+            }
+        }
+
+        if(found == -1)
+        {
+            cout<<"-1"<<endl;
+        }
+    }
+
+    return 0;
+}
