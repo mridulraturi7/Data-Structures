1f3601f9249d20aa291b811efccf171a07488a4c
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-08 11:34:51 +0530

    [Modify] Array/Problem105.cpp

--- Array/Problem105.cpp
+++ Array/Problem105.cpp
@@ -1,5 +1,8 @@
 /*
     Balanced Array.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/balanced-array/0
+    Difficulty - Basic
 */
 
 #include<iostream>
