e487e9d66939db2d76d4aedb86a7f560d99b0905
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-09 20:49:31 +0530

    [Done Problem 113] Array/Problem113.cpp

---
+++ Array/Problem113.cpp
@@ -0,0 +1,48 @@
+/*
+    Count Pair SUm.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int m, n, x;
+        cin>>m>>n;
+
+        int *array1 = new int[m];
+        int *array2 = new int[n];
+        for(int i = 0; i < m; i++)
+        {
+            cin>>array1[i];
+        }
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array2[i];
+        }
+
+        cin>>x;
+
+        int count = 0;
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(array1[i] + array2[j] == x)
+                {
+                    count++;
+                }
+            }
+        }
+
+        cout<<count<<endl;
+    }
+
+    return 0;
+}
