a7e12a31a5b305d8e41397979f18a276ec307629
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-09 20:29:24 +0530

    [Done Problem 111] Array/Problem111.cpp

---
+++ Array/Problem111.cpp
@@ -0,0 +1,43 @@
+/*
+    Pair array product sum.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if((array[i] + array[j]) < (array[i] * array[j]))
+                {
+                    count++;
+                }
+            }
+        }
+
+        cout<<count<<endl;
+
+    }
+
+    return 0;
+}
