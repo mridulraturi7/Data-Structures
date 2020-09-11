9fc78882cf7056b32d2062c968ff3ca2d68986c1
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-11 21:16:15 +0530

    [Done Problem 121] Array/Problem121.cpp

---
+++ Array/Problem121.cpp
@@ -0,0 +1,57 @@
+/*
+    Greater on Right Side.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        void nextGreatest(int array[], int n)
+        {
+            int max = array[n - 1];
+            for(int i = n - 2; i >= 0; i--)
+            {
+                int prev_value = array[i];
+                array[i] = max;
+
+                if(prev_value > max)
+                {
+                    max = prev_value;
+                }
+            }
+            array[n - 1] = -1;
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        Solution s;
+        s.nextGreatest(array, n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cout<<array[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
