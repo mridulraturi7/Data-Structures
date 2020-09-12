ce174555f1b5111d4990788e4a2f9c6d7082f930
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-12 23:37:36 +0530

    [WORK IN PROGRESS : Problem 126] Array/Problem126.cpp

---
+++ Array/Problem127.cpp
@@ -0,0 +1,86 @@
+/*
+    Total distance travelled in an array.
+*/
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        long long distance(int array[], int n)
+        {
+            int left, right;
+            for(int i = 0; i < n; i++)
+            {
+                if(array[i] == 1)
+                {
+                    left = i;
+                    break;
+                }
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                if(array[i] == 2)
+                {
+                    right = i;
+                    break;
+                }
+            }
+
+            long long dis = 0;
+
+            int counter = 2;
+
+            while(right != -1)
+            {
+                dis += abs(left - right);
+
+                left = right;
+
+                right = find(array, n, ++counter);
+            }
+
+            return dis;
+        }
+
+        int find(int array[], int n, int element)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                if(array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+        
+        Solution s;
+
+        cout<<s.distance(array, n)<<endl;
+    }
+
+    return 0;
+}
