9aa61f98e0477429df662d44edd2aa83576e1772
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-14 20:26:54 +0530

    [Done Problem 131] Array/Problem131.cpp

---
+++ Array/Problem131.cpp
@@ -0,0 +1,51 @@
+/*
+    Make Co - Prime Array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int findGCD(int a, int b);
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(findGCD(array[i], array[i + 1]) != 1)
+            {
+                count++;
+            }
+        }
+
+        cout<<count<<endl;
+    }
+
+    return 0;
+}
+
+int findGCD(int a, int b)
+{
+    if(a == 0)
+    {
+        return b;
+    }
+
+    return findGCD(b % a, a);
+}
