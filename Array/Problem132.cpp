982006ed42b1022c95727b7ac9a9ca49622e54bb
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-14 20:35:54 +0530

    [Done Problem 132] Array/Problem132.cpp

---
+++ Array/Problem132.cpp
@@ -0,0 +1,41 @@
+/*
+    Why is Melody so Chocolaty?
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int max_happiness = 0;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            int curremt_happiness = array[i] + array[i + 1];
+
+            if(curremt_happiness > max_happiness)
+            {
+                max_happiness = curremt_happiness;
+            }
+        }
+
+        cout<<max_happiness<<endl;
+    }
+
+    return 0;
+}
