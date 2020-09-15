065baec34f380afc7955c321ea41e7efd47e1ac8
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-15 23:28:54 +0530

    [Done Problem 137] Array/Problem137.cpp

---
+++ Array/Problem137.cpp
@@ -0,0 +1,55 @@
+/*
+    Minimum number to form the sum even.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        int minNumber(long long int array[], int n)
+        {
+            long long int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                sum += array[i];
+            }
+
+            if(sum % 2 == 0)
+            {
+                return 2;
+            }
+
+            else
+            {
+                return 1;
+            }
+            
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        long long int *array = new long long int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        Solution s;
+
+        cout<<s.minNumber(array, n)<<endl;
+    }
+
+    return 0;
+}
