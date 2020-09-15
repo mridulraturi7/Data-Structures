5f809845a1f954abdea6469ddcb6a9fedc68880f
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-15 23:50:55 +0530

    [Done Problem 138] Array/Problem138.cpp

---
+++ Array/Problem138.cpp
@@ -0,0 +1,81 @@
+/*
+    Absolute Distinct Count.
+*/
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        int distinctCount(int array[], int n)
+        {
+            int distinct = n;
+            int l = 0, r = n - 1;
+            while(l < r)
+            {
+                while(l != r && array[l] == array[l + 1])
+                {
+                    distinct--;
+                    l++;
+                }
+
+                while(l != r && array[r] == array[r - 1])
+                {
+                    distinct--;
+                    r--;
+                }
+
+                if(l == r)
+                {
+                    break;
+                }
+
+                if(array[l] + array[r] == 0)
+                {
+                    distinct--;
+                    l++;
+                    r--;
+                }
+
+                else if(array[l] + array[r] < 0)
+                {
+                    l++;
+                }
+
+                else
+                {
+                    r--;
+                }
+                
+            }
+
+            return distinct;
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        Solution s;
+
+        cout<<s.distinctCount(array, n)<<endl;
+    }
+
+    return 0;
+}
