01b91facd5d03c9617e6817bf715ab5a62d37eb7
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-16 21:35:27 +0530

    [Done Problem 139] Array/Problem139.cpp

---
+++ Array/Problem139.cpp
@@ -0,0 +1,58 @@
+/*
+    Reverse Sub Array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        void reverseSubArray(int array[], int l, int r)
+        {
+            int left = l - 1;
+            int right = r - 1;
+            while(left < right)
+            {
+                array[left] = array[left] + array[right];
+                array[right] = array[left] - array[right];
+                array[left] = array[left] - array[right];
+                left++;
+                right--;
+            }
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+    
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int l, r;
+        cin>>l>>r;
+
+        Solution s;
+        s.reverseSubArray(array, l, r);
+
+        for(int i = 0; i < n; i++)
+        {
+            cout<<array[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
