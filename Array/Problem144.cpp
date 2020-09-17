df68bb52355141e94e8be0770a62a6d80925d601
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-17 23:56:54 +0530

    [WIP Problem 144] Array/Problem144.cpp

---
+++ Array/Problem144.cpp
@@ -0,0 +1,25 @@
+/*
+    King's War.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+    }
+}
