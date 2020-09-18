ea202398412f3d5fe5faa7895fbe57499ca3cb08
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-18 12:29:26 +0530

    [Done Problem 146] Array/Problem146.cpp

---
+++ Array/Problem146.cpp
@@ -0,0 +1,66 @@
+/*
+    Countries at War.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int countryA[n], countryB[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>countryA[i];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cin>>countryB[i];
+        }
+
+        int countA = 0, countB = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(countryA[i] > countryB[i])
+            {
+                countA++;
+            }
+
+            else if(countryA[i] < countryB[i])
+            {
+                countB++;
+            }
+        }
+
+        cout<<countA<<" "<<countB<<" ";
+
+        if(countA > countB)
+        {
+            cout<<"A";
+        }
+
+        else if(countA < countB)
+        {
+            cout<<"B";
+        }
+
+        else
+        {
+            cout<<"DRAW";
+        }
+
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
