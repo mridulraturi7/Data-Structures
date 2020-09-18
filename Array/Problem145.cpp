857e428c12a806bf3a9f9d35ea61c992423b2943
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-18 09:25:33 +0530

    [WIP Problem 145] Array/Problem145.cpp

---
+++ Array/Problem145.cpp
@@ -0,0 +1,42 @@
+/*
+    Maximum Perimeter of Triangle from array.
+*/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int maxPerimeter = -1;
+
+        sort(array, array + n);
+        for(int i = n - 1; i > 1; i--)
+        {
+            if(array[i - 1] + array[i - 2] > array[i] && array[i - 2] + array[i] > array[i - 1] && array[i - 1] + array[i] > array[i - 2])
+            {
+                maxPerimeter = array[i] + array[i - 1] + array[i - 2];
+                break;
+            }
+        }
+
+        cout<<maxPerimeter<<endl;
+    }
+
+    return 0;
+}
