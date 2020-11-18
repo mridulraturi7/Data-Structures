094d98d98081c15bbae6f0961be733b329497ba3
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-18 23:07:05 +0530

    [Implemented] Tree/BinaryTree/SimpleTree.cpp

--- Tree/Binary Tree/SimpleTree.cpp
+++ Tree/Binary Tree/SimpleTree.cpp
@@ -9,3 +9,33 @@
                /
               4
 */
+
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+    //Create root node.
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+
+    return 0;
+}
