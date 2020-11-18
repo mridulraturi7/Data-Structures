12e2b726f670f19f5c5ff27e5153c04353ef2d83
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-18 23:01:45 +0530

    [Create] Tree/BinaryTree/SimpleTree.cpp

---
+++ Tree/Binary Tree/SimpleTree.cpp
@@ -0,0 +1,11 @@
+/*
+    Create a simple binary tree as following:
+                    
+                    1
+                   / \
+                  /   \
+                 2     3
+                /
+               /
+              4
+*/
