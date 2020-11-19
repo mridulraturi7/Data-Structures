1400dad906878ff337d70db1b3ea72dc07902354
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-19 19:23:43 +0530

    [Working] Tree/BinaryTree/Insertion.cpp

---
+++ Tree/Binary Tree/Insertion.cpp
@@ -0,0 +1,101 @@
+/*
+    Insertion in a Binary Tree.
+*/
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+Node* createNode(int data)
+{
+    Node *newNode = new Node();
+    if(!newNode)
+    {
+        cout<<"Memory Error!!"<<endl;
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+Node* insertNode(Node *root, int data)
+{
+    if(root == NULL)
+    {
+        root = createNode(data);
+        return root;
+    }
+
+    queue<struct Node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if(temp->left != NULL)
+        {
+            q.push(temp->left);
+        }
+        else
+        {
+            temp->left = createNode(data);
+            return root;
+        }
+
+        if(temp->right != NULL)
+        {
+            q.push(temp->right);
+        }
+        else
+        {
+            temp->right = createNode(data);
+            return root;
+        }
+    }
+}
+
+void inorder(Node *temp)
+{
+    inorder(temp->left);
+    cout<<temp->data<<" ";
+    inorder(temp->right);
+}
+
+int main()
+{
+    Node *root = createNode(10);
+    root->left = createNode(11);
+    root->left->left = createNode(7);
+
+    root->right = createNode(9);
+    root->right->left = createNode(15);
+    root->right->right = createNode(8);
+
+    cout<<"Inorder Traversal Before Insertion: ";
+    inorder(root);
+
+    int key;
+    cout<<"Enter node key to insert: ";
+    cin>>key;
+
+    root = insertNode(root, key);
+
+    cout<<"Inorder Traversal After Inertion: ";
+    inorder(root);
+
+    return 0;
+}
