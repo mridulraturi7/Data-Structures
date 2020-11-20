5c5affd3ec9c74c951a2ce24b248e7ff244d8010
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-20 23:14:03 +0530

    [Done postorder.cpp] Tree/BinaryTree/Traversal/DFS/postorder.cpp

--- Tree/Binary Tree/Traversal/DFS/postorder.cpp
+++ Tree/Binary Tree/Traversal/DFS/postorder.cpp
@@ -6,8 +6,50 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+Node* createNode(int data)
+{
+    struct Node *newNode = new Node();
+    if(!newNode)
+    {
+        cout<<"Memory Error";
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
+
+void postorder(Node *temp)
+{
+    if(temp == NULL)
+    {
+        return;
+    }
+
+    postorder(temp->left);
+    postorder(temp->right);
+    cout<<temp->data<<" ";
+}
+
 int main()
 {
-    
+    struct Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    cout<<"Postorder Traversal of the Binary Tree: ";
+    postorder(root);
+
     return 0;
 }
