19165c29ddc7a02f9bb6f854b1e1df9ec0926bf3
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-21 19:40:52 +0530

    [Create levelorder.cpp] Tree/BinaryTree/Traversal/BFS/levelorder.cpp

---
+++ Tree/Binary Tree/Traversal/BFS/levelorder.cpp
@@ -0,0 +1,33 @@
+/*
+    Level Order Traversal of a Binary Tree.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+Node* createNode(int data)
+{
+    struct Node *newNode = new Node();
+    if(!newNode)
+    {
+        cout<<"Memory Error!!";
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
+
+int main()
+{
+    struct Node *root =   
+}
