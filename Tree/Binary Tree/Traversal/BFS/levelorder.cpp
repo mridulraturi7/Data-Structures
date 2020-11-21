4fa227b10d350a1654e1baca4883fa1037695e2c
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-21 19:47:19 +0530

    [DONE levelorder.cpp] Tree/BinaryTree/Traversal/BFS/levelorder.cpp

--- Tree/Binary Tree/Traversal/BFS/levelorder.cpp
+++ Tree/Binary Tree/Traversal/BFS/levelorder.cpp
@@ -3,6 +3,7 @@
 */
 
 #include<iostream>
+#include<queue>
 
 using namespace std;
 
@@ -27,7 +28,48 @@ Node* createNode(int data)
     newNode->right = NULL;
 }
 
+void levelOrder(struct Node *node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+
+    queue<struct Node*> q;
+    q.push(node);
+
+    struct Node *temp;
+
+    while(!q.empty())
+    {
+        temp = q.front();
+        q.pop();
+
+        cout<<temp->data<<" ";
+
+        if(temp->left != NULL)
+        {
+            q.push(temp->left);
+        }
+
+        if(temp->right != NULL)
+        {
+            q.push(temp->right);
+        }
+    }
+}
+
 int main()
 {
-    struct Node *root =   
+    struct Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    cout<<"Level Order Traversal of the Binary Tree: ";
+    levelOrder(root);
+
+    return 0;
 }
