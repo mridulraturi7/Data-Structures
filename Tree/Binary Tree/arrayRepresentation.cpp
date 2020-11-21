185ca19481a091054fb9c3f7ffa93b6fcd9f15eb
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-21 21:26:20 +0530

    [Done] Tree/BinaryTree/arrayRepresentation.cpp

--- Tree/Binary Tree/arrayRepresentation.cpp
+++ Tree/Binary Tree/arrayRepresentation.cpp
@@ -6,8 +6,73 @@
 
 using namespace std;
 
+char tree[10];
+
+void setRoot(char key)
+{
+    if(tree[0] != '\0')
+    {
+        cout<<"Tree already have root.";
+    }
+    else
+    {
+        tree[0] = key;
+    }
+}
+
+void set_left_child(char key, int parent)
+{
+    if(tree[parent] == '\0')
+    {
+        cout<<"No Parent Found!!";
+    }
+    else
+    {
+        tree[(2 * parent) + 1] = key;
+    }
+}
+
+void set_right_child(char key, int parent)
+{
+    if(tree[parent] == '\0')
+    {
+        cout<<"No Parent Found!!";
+    }
+
+    else
+    {
+        tree[(2 * parent) + 2] = key;
+    }
+}
+
+void printTree()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        if(tree[i] != '\0')
+        {
+            cout<<tree[i];
+        }
+        else
+        {
+            cout<<"-";
+        }
+    }
+}
+
 int main()
 {
+    setRoot('A');
+    set_left_child('B', 0);
+    set_right_child('C', 0);
+
+    set_left_child('D', 1);
+    set_right_child('E', 1);
+
+    set_right_child('F', 2);
+
+    cout<<"Traversal of Binary Tree: ";
+    printTree();
 
     return 0;
 }
