1684115fed53c7c6b610747c9f6d89a50c3f0b8e
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-21 20:37:52 +0530

    [Done] Tree/BinaryTree/height.cpp

--- Tree/Binary Tree/height.cpp
+++ Tree/Binary Tree/height.cpp
@@ -13,7 +13,54 @@ struct Node
     struct Node *right;
 };
 
+Node* createNode(int data)
+{
+    struct Node *newNode = new Node();
+    if(newNode == NULL)
+    {
+        cout<<"Memory Error!!";
+        return NULL;
+    }
+    
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
+
+int height(struct Node *node)
+{
+    if(node == NULL)
+    {
+        return 0;
+    }
+
+    else
+    {
+        int lheight = height(node->left);
+        int rhieght = height(node->right);
+
+        if(lheight > rhieght)
+        {
+            return lheight + 1;
+        }
+
+        else
+        {
+            return rhieght + 1;
+        }   
+    }
+}
+
 int main()
 {
+    struct Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
 
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    cout<<"Height of the Binary Tree is: "<<height(root);
+
+    return 0;
 }
