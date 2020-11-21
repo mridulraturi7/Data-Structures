19e86a13917273c58c6451cf180706a9740728d3
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-21 20:32:41 +0530

    [Create height.cpp] Tree/BinaryTree/height.cpp

---
+++ Tree/Binary Tree/height.cpp
@@ -0,0 +1,19 @@
+/*
+    Height or Maximum Depth of a Binary Tree.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+int main()
+{
+
+}
