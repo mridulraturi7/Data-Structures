042615ba3370068e4a96bfc671cbea4b1a46ed60
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-22 12:31:19 +0530

    [Done] Tree/BinaryTree/Traversal/DFS/Iterative/preorder.cpp

--- Tree/Binary Tree/Traversal/DFS/Iterative/preorder.cpp
+++ Tree/Binary Tree/Traversal/DFS/Iterative/preorder.cpp
@@ -3,11 +3,71 @@
 */
 
 #include<iostream>
+#include<stack>
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+Node* createNode(int data)
+{
+    struct Node *newNode = new Node();
+    if(!newNode)
+    {
+        cout<<"Memory Error!!";
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
+
+void preorder(struct Node *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    stack<struct Node*> s;
+    s.push(root);
+
+    while(s.empty() == false)
+    {
+        struct Node *current = s.top();
+        s.pop();
+
+        cout<<current->data<<" ";
+
+        if(current->right != NULL)
+        {
+            s.push(current->right);
+        }
+
+        if(current->left != NULL)
+        {
+            s.push(current->left);
+        }
+    }
+}
+
 int main()
 {
+    struct Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    cout<<"Preorder Traversal of the Binary Tree: ";
+    preorder(root);
 
     return 0;
 }
