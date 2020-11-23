1de55836efa40ce717e0e096b89dace8276a4a3d
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-23 22:34:55 +0530

    [Create] Tree/BinaryTree/postorderFromInorderPreorder.cpp

---
+++ Tree/Binary Tree/postorderFromInorderPreorder.cpp
@@ -0,0 +1,15 @@
+/*
+    Find the postorder traversal of a binary tree given its inorder and preorder traversal.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    
+    return 0;
+}
