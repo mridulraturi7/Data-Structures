20ab7aab6b687b5c4be9775df768522826076937
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-24 21:26:08 +0530

    [Done] Tree/BinarySearchTree/Searching/Recursive.cpp

--- Tree/Binary Search Tree/Searching/Recursive.cpp
+++ Tree/Binary Search Tree/Searching/Recursive.cpp
@@ -1,3 +1,74 @@
 /*
     Searching in Binary Search Tree : Recursive Approach.
 */
+
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+Node* createNode(int data)
+{
+    struct Node *newNode = new Node();
+    if(!newNode)
+    {
+        cout<<"Memory Error!!";
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
+
+Node* search(struct Node* root, int key)
+{
+    if(root == NULL || root->data == key)
+    {
+        return root;
+    }
+
+    if(root->data > key)
+    {
+        return search(root->left, key);
+    }
+
+    return search(root->right, key);
+}
+
+int main()
+{
+    struct Node *root = createNode(8);
+    root->left = createNode(5);
+    root->right = createNode(10);
+
+    root->left->left = createNode(2);
+    root->left->right = createNode(6);
+
+    root->right->left = createNode(9);
+    root->right->right = createNode(12);
+
+    int key;
+    cout<<"Enter the key of the node to search: ";
+    cin>>key;
+
+    struct Node *result = search(root, key);
+
+    if(!result)
+    {
+        cout<<"Node with key "<<key<<" is not present in the BST.";
+    }
+
+    else
+    {
+        cout<<"Node with key "<<key<<" is present in the BST.";
+    }
+
+    return 0;
+}
