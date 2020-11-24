2fbd6931a8caafcb9301b9c10e958aa4d86ad70f
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-24 20:04:03 +0530

    [Done] Tree/BinarySearchTree/BasicBST.cpp

--- Tree/Binary Search Tree/BasicBST.cpp
+++ Tree/Binary Search Tree/BasicBST.cpp
@@ -6,7 +6,32 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
 int main()
 {
-    
+    struct Node *root = new Node(8);
+
+    root->left = new Node(5);
+    root->right = new Node(10);
+
+    root->left->left = new Node(2);
+    root->left->right = new Node(6);
+
+    root->right->left = new Node(9);
+    root->right->right = new Node(12);
+
+    return 0;
 }
