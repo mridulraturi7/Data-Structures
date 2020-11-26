519c4d9baf2d3a76161f3eef682e0eb2c6dd90b4
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-26 11:37:07 +0530

    [Modify Logic] Tree/BinarySearchTree/Insertion/Iterative.cpp

--- Tree/Binary Search Tree/Insertion/Iterative.cpp
+++ Tree/Binary Search Tree/Insertion/Iterative.cpp
@@ -27,12 +27,12 @@ Node* createNode(int data)
     newNode->right = NULL;
 }
 
-void insert(struct Node *root, int key)
+Node* insert(struct Node *root, int key)
 {
     if(root == NULL)
     {
         root = createNode(key);
-        return;
+        return root;
     }
 
     struct Node* current = root;
@@ -62,6 +62,20 @@ void insert(struct Node *root, int key)
     {
         prev_node->right = createNode(key);
     }
+
+    return root;
+}
+
+void inorder(struct Node *node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+
+    inorder(node->left);
+    cout<<node->data<<" ";
+    inorder(node->right);
 }
 
 int main()
@@ -76,11 +90,18 @@ int main()
     root->right->left = createNode(9);
     root->right->right = createNode(12);
 
+    cout<<"Inorder Traversal before inserting new node: ";
+    inorder(root);
+    cout<<endl;
+
     int key;
     cout<<"Enter node's key to insert in the BST: ";
     cin>>key;
 
-    insert(root, key);
+    root = insert(root, key);
+
+    cout<<"Inorder Traversal after inserting new node: ";
+    inorder(root);
 
     return 0;
 }
