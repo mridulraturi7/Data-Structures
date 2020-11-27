106ca58c91b5daa6956305845b89446a9dcb3a67
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-27 22:36:44 +0530

    [ADD Comments] Graph/Representation/AdjacencyList/Implementation.cpp

--- Graph/Representation/Adjacency List/Implementation.cpp
+++ Graph/Representation/Adjacency List/Implementation.cpp
@@ -1,5 +1,21 @@
 /*
+    Implementation 2.
+
     Adjacency List Representation of a Graph.
+
+    Construct the Adjacency List of the following Graph:
+
+                    0---------1
+                    |         |
+                    |         |
+                    |         |
+                    |         |
+                    2---------3
+                     \       /
+                      \     /
+                       \   /
+                        \ /
+                         4
 */
 
 #include<iostream>
