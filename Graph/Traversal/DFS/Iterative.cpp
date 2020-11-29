56c02d36bb26fdc52810595d130d9f3faecff4b1
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-29 22:59:38 +0530

    [Modify] Graph/Traversal/DFS/Iterative.cpp

--- Graph/Traversal/DFS/Iterative.cpp
+++ Graph/Traversal/DFS/Iterative.cpp
@@ -89,24 +89,11 @@ int main()
 {
     Graph G(5);
 
-    G.addEdge(0, 1);
-    G.addEdge(0, 2);
-    G.addEdge(0, 3);
-
-    G.addEdge(1, 0);
-    G.addEdge(1, 3);
-
-    G.addEdge(2, 0);
-    G.addEdge(2, 3);
-    G.addEdge(2, 4);
-
-    G.addEdge(3, 0);
-    G.addEdge(3, 1);
-    G.addEdge(3, 2);
-    G.addEdge(3, 4);
-
-    G.addEdge(4, 2);
-    G.addEdge(4, 3);
+    G.addEdge(1, 0); 
+    G.addEdge(0, 2); 
+    G.addEdge(2, 1); 
+    G.addEdge(0, 3); 
+    G.addEdge(1, 4);
 
     cout<<"Graph Status (Adjacency Matrix): "<<endl;
     G.printGraph();
