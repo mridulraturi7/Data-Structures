7bcc28c6fcbf72122464119e29ea4e8adf411bfe
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-29 22:42:11 +0530

    [Create Class] Graph/Traversal/DFS/Iterative.cpp

--- Graph/Traversal/DFS/Iterative.cpp
+++ Graph/Traversal/DFS/Iterative.cpp
@@ -3,11 +3,41 @@
 */
 
 #include<iostream>
+#include<list>
+#include<stack>
 
 using namespace std;
 
+class Graph
+{
+    int V;
+
+    list<int> *adj;
+
+    public:
+        Graph()
+        {
+
+        }
+
+        Graph(int n);
+
+        void addEdge(int u, int v);
+
+        void dfs(int s);
+
+        void printGraph();
+};
+
+Graph::Graph(int n)
+{
+    this->V = n;
+
+    adj = new list<int>[V];
+}
+
 int main()
 {
-    
+
     return 0;
 }
