70d3fb35eff06a2b0cc22c3f6e31bdd4bb0a37b8
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-05 18:58:09 +0530

    [Done] String/Problem1.exe

--- String/Problem1.cpp
+++ String/Problem1.cpp
@@ -3,11 +3,40 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+class Solution
+{
+    public:
+        string toLower(string S)
+        {
+            for(int i = 0; i < S.length(); i++)
+            {
+                if(S[i] < 91)
+                {
+                    S[i] = S[i] + 32;
+                }
+            }
+
+            return S;
+        }
+};
+
 int main()
 {
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string S;
+        cin>>S;
+
+        Solution ob;
+        cout<<ob.toLower(S)<<endl;
+    }
 
     return 0;
 }
