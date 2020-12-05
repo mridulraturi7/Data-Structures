b6799fbd16e4b393dae922bc856221a9716b5d53
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-05 19:04:28 +0530

    [Done] String/Problem2.exe

--- String/Problem2.cpp
+++ String/Problem2.cpp
@@ -3,8 +3,32 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
+
+class Solution
+{
+    public:
+        string revStr(string S)
+        {
+            int l = 0;
+            int r = S.length() - 1;
+
+            while(l < r)
+            {
+                char temp;
+                temp = S[l];
+                S[l] = S[r];
+                S[r] = temp;
+
+                l++;
+                r--;
+            }
+
+            return S;
+        }
+};
 
 int main()
 {
@@ -13,7 +37,11 @@ int main()
 
     while(testCase-- != 0)
     {
+        string S;
+        cin>>S;
 
+        Solution ob;
+        cout<<ob.revStr(S)<<endl;
     }
 
     return 0;
