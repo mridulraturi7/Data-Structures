4d6db9005b9f3200c99fe79793e8277f4d020033
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-05 19:17:07 +0530

    [Create] String/Problem5.cpp

---
+++ String/Problem5.cpp
@@ -0,0 +1,20 @@
+/*
+    Convert a list of characters into a string.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
