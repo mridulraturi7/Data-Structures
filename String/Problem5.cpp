215dc9d0bd5559543dcc2ae6418341acc11a5bea
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-05 19:22:37 +0530

    [Done] String/Problem5.exe

--- String/Problem5.cpp
+++ String/Problem5.cpp
@@ -3,8 +3,24 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
+
+class Solution
+{
+    public:
+        string chartostr(char ch[], int n)
+        {
+            string result;
+            for(int i = 0; i < n; i++)
+            {
+                result += ch[i];
+            }
+
+            return result;
+        }
+};
 
 int main()
 {
@@ -13,7 +29,17 @@ int main()
 
     while(testCase-- != 0)
     {
+        int n;
+        cin>>n;
 
+        char *ch = new char[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>ch[i];
+        }
+
+        Solution ob;
+        cout<<ob.chartostr(ch, n)<<endl;
     }
 
     return 0;
