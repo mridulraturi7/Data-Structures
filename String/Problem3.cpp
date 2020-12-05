f13f989caf960ff39a94d66aa12e4853c061595e
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-05 19:10:47 +0530

    [Done] String/Problem3.exe

--- String/Problem3.cpp
+++ String/Problem3.cpp
@@ -12,7 +12,21 @@ class Solution
     public:
         int isPalindrome(string S)
         {
+            int l = 0;
+            int r = S.length() - 1;
 
+            while(l < r)
+            {
+                if(S[l] != S[r])
+                {
+                    return 0;
+                }
+
+                l++;
+                r--;
+            }
+
+            return 1;
         }
 };
 
