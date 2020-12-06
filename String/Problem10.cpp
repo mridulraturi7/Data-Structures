3148dd4ef08c82d116b43d37751866e336fca2b4
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 19:53:36 +0530

    [Done] String/Problem10.cpp

--- String/Problem10.cpp
+++ String/Problem10.cpp
@@ -14,7 +14,18 @@ int main()
 
     while(testCase-- != 0)
     {
-        
+        string S;
+        cin>>S;
+
+        int l, r;
+        cin>>l>>r;
+
+        for(int i = l; i <= r; i++)
+        {
+            cout<<S[i];
+        }
+
+        cout<<endl;
     }
 
     return 0;
