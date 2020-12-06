8595ab449a05f7348c95129356ad86a8d2440ab3
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 20:02:30 +0530

    [Modify] String/Problem11.cpp

--- String/Problem11.cpp
+++ String/Problem11.cpp
@@ -1,5 +1,8 @@
 /*
     Lower Case to Upper Case.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/lower-case-to-upper-case/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1
+    Difficulty - School
 */
 
 #include<iostream>
