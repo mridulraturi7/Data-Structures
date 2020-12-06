006b0be451cd461ee90211000e9b6d057a6e48fb
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 19:22:02 +0530

    [Modify] String/Problem7.cpp

--- String/Problem7.cpp
+++ String/Problem7.cpp
@@ -1,5 +1,8 @@
 /*
     Delete Alternate Characters.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/java-delete-alternate-characters/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1
+    Difficulty - School
 */
 
 #include<iostream>
