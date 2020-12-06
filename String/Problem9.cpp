1acc0b103c06adf1226686357843939f05ec2004
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 19:44:51 +0530

    [Done] String/Problem9.cpp

--- String/Problem9.cpp
+++ String/Problem9.cpp
@@ -7,6 +7,19 @@
 
 using namespace std;
 
+void printPattern(string str)
+{
+    int len = str.length();
+    for(int i = 0; i < len; i++)
+    {
+        for(int j = 0; j < len - i; j++)
+        {
+            cout<<str[j];
+        }
+        cout<<"\n";
+    }
+}
+
 int main()
 {
     int testCase;
@@ -14,7 +27,10 @@ int main()
 
     while(testCase-- != 0)
     {
+        string str;
+        cin>>str;
 
+        printPattern(str);
     }
 
     return 0;
