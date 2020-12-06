a083824f675fe3dbb055a154b2df47f47bd2dd5b
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 18:47:32 +0530

    [Create] String/Problem7.cpp

---
+++ String/Problem7.cpp
@@ -0,0 +1,21 @@
+/*
+    Delete Alternate Characters.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string S;
+        cin>>S;
+    }
+
+    return 0;
+}
