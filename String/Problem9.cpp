8ca49e6f246c8c2ffa8a03c4bb6348a3f3567ad2
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 19:45:25 +0530

    [Modify] String/Problem9.cpp

--- String/Problem9.cpp
+++ String/Problem9.cpp
@@ -1,5 +1,8 @@
 /*
     Pattern of Strings.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/pattern-of-strings/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1
+    Difficulty - School
 */
 
 #include<iostream>
