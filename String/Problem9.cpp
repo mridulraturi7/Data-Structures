c62851ec56a7144838f760a3aadeb16308b752ed
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 19:41:36 +0530

    [Create] String/Problem9.cpp

---
+++ String/Problem9.cpp
@@ -0,0 +1,21 @@
+/*
+    Pattern of Strings.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
