9be1079e147443428c65b62f25e872b7473ffdb1
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-06 19:59:13 +0530

    [Create] String/Problem11.cpp

---
+++ String/Problem11.cpp
@@ -0,0 +1,20 @@
+/*
+    Lower Case to Upper Case.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
