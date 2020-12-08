83d1e13000a3d0c2ab35046402f98ff57c5f9414
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-08 19:14:44 +0530

    [Create] String/Problem14.cpp

---
+++ String/Problem14.cpp
@@ -0,0 +1,20 @@
+/*
+    Count the alphabets.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
