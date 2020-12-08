5b2b50c9916f2daf60bfd9f3c8d213f9112aee74
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-08 19:23:52 +0530

    [Done] String/Problem15.cpp

--- String/Problem15.cpp
+++ String/Problem15.cpp
@@ -3,6 +3,7 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -13,7 +14,14 @@ int main()
 
     while(testCase-- != 0)
     {
-        
+        string str1, str2;
+        cin>>str1>>str2;
+
+        str1.length() >= str2.length() ? cout<<str1.length() : cout<<str2.length();
+        cout<<endl;
+
+        str1.append(str2);
+        cout<<str1<<endl;
     }
 
     return 0;
