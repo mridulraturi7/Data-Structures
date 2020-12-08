c05ce78a1d59be2fcc94936116734259a803b1a8
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-08 19:07:06 +0530

    [Create] String/Problem13.cpp

---
+++ String/Problem13.cpp
@@ -0,0 +1,22 @@
+/*
+    Change the string.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string str;
+        cin>>str;
+    }
+
+    return 0;
+}
