65e716520db21ff5c4063bdbd4cb5b8f2fbb5db8
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-08 19:19:26 +0530

    [Create] String/Problem15.cpp

---
+++ String/Problem15.cpp
@@ -0,0 +1,20 @@
+/*
+    C++ Strings.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
