50d3c950b1fe7f9f226cbbd689527e7b1acbbba3
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-09 20:16:28 +0530

    [Modify] String/Problem18.cpp

--- String/Problem18.cpp
+++ String/Problem18.cpp
@@ -15,13 +15,7 @@ class Solution
     public:
         bool check(string str)
         {
-            for(long i = 0; i < str.length() - 1; i++)
-            {
-                if(str[i] != str[i + 1])
-                {
-                    return false;
-                }
-            }
+            
 
             return true;
         }
