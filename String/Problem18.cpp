6347474f5feba293ca190d3d6acbad8aac928887
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-09 19:58:26 +0530

    [Create] String/Problem18.cpp

---
+++ String/Problem18.cpp
@@ -0,0 +1,24 @@
+/*
+    Check String.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string str;
+        cin>>str;
+
+
+    }
+
+    return 0;
+}
