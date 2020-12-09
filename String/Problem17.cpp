0bacadb5fe438c43c2acbdac99e6fa724b3f8ae4
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-09 19:43:28 +0530

    [Done] String/Problem17.cpp

--- String/Problem17.cpp
+++ String/Problem17.cpp
@@ -7,6 +7,34 @@
 
 using namespace std;
 
+string reverseVowels(string str)
+{
+    int l = 0, r = str.length() - 1;
+
+    while(l < r)
+    {
+        if((str[l] == 'a' || str[l] == 'e' || str[l] == 'i' || str[l] == 'o', str[l] == 'u') && (str[r] == 'a' || str[r] == 'e' || str[r] == 'i', str[r] == 'o', str[r] == 'u'))
+        {
+            char temp;
+            temp = str[l];
+            str[l] = str[r];
+            str[r] = temp;
+        }
+
+        else if(str[l] != 'a' && str[l] != 'e' && str[l] != 'i' && str[l] != 'o' && str[l] != 'u')
+        {
+            l++;
+        }
+
+        else if(str[r] != 'a' && str[r] != 'e' && str[r] != 'i' && str[r] != 'o' && str[r] != 'u')
+        {
+            r--;
+        }
+    }
+
+    return str;
+}
+
 int main()
 {
     int testCase;
@@ -14,7 +42,12 @@ int main()
 
     while(testCase-- != 0)
     {
-        
+        string str;
+        cin>>str;
+
+        string res = reverseVowels(str);
+
+        cout<<res<<endl;
     }
 
     return 0;
