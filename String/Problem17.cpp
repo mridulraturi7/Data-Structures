e6bd05ac12195607d4fcc5f21e89cae62361b1f5
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-09 19:36:22 +0530

    [Create] String/Problem17.cpp

---
+++ String/Problem17.cpp
@@ -0,0 +1,21 @@
+/*
+    Reversing the vowels.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
