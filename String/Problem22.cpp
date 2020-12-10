577a258c065c69ed61ef3906dde47b69a68df2f2
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-10 20:35:15 +0530

    [Create] String/Problem22.cpp

---
+++ String/Problem22.cpp
@@ -0,0 +1,25 @@
+/*
+    Upper Case Conversion.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    cin.ignore();
+
+    while(testCase-- != 0)
+    {
+        
+
+        cout<<str<<endl;
+    }
+
+    return 0;
+}
