bbc1daf093e94f83afd784ce824db30e59a59420
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-10 20:22:48 +0530

    [Done] String/Problem21.cpp

--- String/Problem21.cpp
+++ String/Problem21.cpp
@@ -1,5 +1,8 @@
 /*
     Triangle shrinking downwards.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/triangle-shrinking-downwards/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1
+    Difficulty - School
 */
 
 #include<iostream>
