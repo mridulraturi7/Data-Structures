ffd30a946ee7b14ee58620a4a19a15007d6c4600
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-10 19:54:49 +0530

    [Create] String/Problem20.cpp

---
+++ String/Problem20.cpp
@@ -0,0 +1,20 @@
+/*
+    Check for Binary.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
