9dafc1ce1d7bc1d438904c05ff4751d3b4414ba8
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-10 20:38:35 +0530

    [Modify] String/Problem22.cpp

--- String/Problem22.cpp
+++ String/Problem22.cpp
@@ -1,5 +1,8 @@
 /*
     Upper Case Conversion.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/upper-case-conversion/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1#
+    Difficulty - School
 */
 
 #include<iostream>
