3fb0634c9f8b97fb4b0b33550b19abe79e5b4217
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-11 21:00:46 +0530

    [Modify] String/Problem23.cpp

--- String/Problem23.cpp
+++ String/Problem23.cpp
@@ -1,5 +1,8 @@
 /*
     Remove the vowels from a string.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/remove-vowels-from-string1446/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1
+    Difficulty - School
 */
 
 
