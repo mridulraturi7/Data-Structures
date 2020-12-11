5f10ce313a105bd9ed6a4e0fc0d6acb66aa393d5
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-11 20:50:17 +0530

    [Create] String/Problem23.cpp

---
+++ String/Problem23.cpp
@@ -0,0 +1,21 @@
+/*
+    Remove the vowels from a string.
+*/
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
