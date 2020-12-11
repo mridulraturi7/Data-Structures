59b8bb18ef4aee924f4c8dcf309cb100a5172fe2
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-11 21:31:09 +0530

    [Modify] String/Problem25.cpp

--- String/Problem25.cpp
+++ String/Problem25.cpp
@@ -1,5 +1,8 @@
 /*
     Extract the integers.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/extract-the-integers/0/?category[]=Strings&difficulty[]=-2&page=1&query=category[]Stringsdifficulty[]-2page1#
+    Difficulty - School
 */
 
 #include<iostream>
