b0c99d254dfeae46cedbd5f5a72c69cd4efd5985
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-11 21:05:56 +0530

    [Done] String/Problem24.cpp

--- String/Problem24.cpp
+++ String/Problem24.cpp
@@ -12,7 +12,16 @@ class Solution
     public:
         string removeSpaces(string str)
         {
-            
+            string res;
+            for(int i = 0; i < str.length(); i++)
+            {
+                if(str[i] != ' ')
+                {
+                    res.push_back(str[i]);
+                }
+            }
+
+            return res;
         }
 };
 
@@ -21,9 +30,15 @@ int main()
     int testCase;
     cin>>testCase;
 
+    cin.ignore();
+
     while(testCase-- != 0)
     {
+        string str;
+        getline(cin, str);
 
+        Solution ob;
+        cout<<ob.removeSpaces(str)<<endl;
     }
 
     return 0;
