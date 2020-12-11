dbb7d29c38af7ae85b7c269370f01c0f3a110d04
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-11 21:08:26 +0530

    [Create] String/Problem25.cpp

---
+++ String/Problem25.cpp
@@ -0,0 +1,21 @@
+/*
+    Extract the integers.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
