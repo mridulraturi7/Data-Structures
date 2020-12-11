aac615d0c96fc2f1dab9aa18697db1e05001176d
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-11 21:02:23 +0530

    [Create] String/Problem24.cpp

---
+++ String/Problem24.cpp
@@ -0,0 +1,21 @@
+/*
+    Remove Spaces.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
