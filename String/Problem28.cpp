580c3b8f114e3c74fcc2fc9104bdab8bd2e46207
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 23:05:22 +0530

    [Modify] String/Problem28.cpp

--- String/Problem28.cpp
+++ String/Problem28.cpp
@@ -1,5 +1,8 @@
 /*
     Red Or Green.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/red-or-green5711/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
+    Difficulty - Basic
 */
 
 #include<iostream>
