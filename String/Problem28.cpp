d1915cb36c83e3658d5e906d6960fd76a226d0ba
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 23:01:52 +0530

    [Create] String/Problem28.cpp

---
+++ String/Problem28.cpp
@@ -0,0 +1,31 @@
+/*
+    Red Or Green.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        int RedOrGreen(string str)
+        {
+            
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string str;
+        cin>>str;
+    }
+
+    return 0;
+}
