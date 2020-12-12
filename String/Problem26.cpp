a5fe9c84b456c23368e7a297e031130246e2db1c
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 22:47:14 +0530

    [Done] String/Problem26.cpp

--- String/Problem26.cpp
+++ String/Problem26.cpp
@@ -3,6 +3,7 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -13,7 +14,25 @@ int main()
 
     while(testCase-- != 0)
     {
-        
+        string str1, str2;
+        cin>>str1>>str2;
+
+        string res;
+        res = str1 + str2;
+
+        int l = 0, r = res.length() - 1;
+
+        while(l < r)
+        {
+            char temp = res[l];
+            res[l] = res[r];
+            res[r] = temp;
+
+            l++;
+            r--;
+        }
+
+        cout<<res<<endl;
     }
 
     return 0;
