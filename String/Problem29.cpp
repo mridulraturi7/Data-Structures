ef909b56efa6894656e5ea76dc4e00b12406e051
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 23:12:20 +0530

    [Create] String/Problem29.cpp

---
+++ String/Problem29.cpp
@@ -0,0 +1,20 @@
+/*
+    Sum of numbers in string.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
