b89ba6767a34cf77949970f09512b6b920f21879
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 23:16:23 +0530

    [Done] String/Problem29.cpp

--- String/Problem29.cpp
+++ String/Problem29.cpp
@@ -3,8 +3,32 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
+
+int findSum(string str)
+{
+    string digit = "0";
+    int sum = 0;
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(isdigit(str[i]))
+        {
+            for(int j = i; isdigit(str[j]); j++)
+            {
+                digit += str[j];
+                i = j;
+            }
+        }
+
+        sum += stoi(digit);
+
+        digit = "0";
+    }
+
+    return sum;
+}
 
 int main()
 {
@@ -13,7 +37,10 @@ int main()
 
     while(testCase-- != 0)
     {
-        
+        string str;
+        cin>>str;
+
+        cout<<findSum(str)<<endl;
     }
 
     return 0;
