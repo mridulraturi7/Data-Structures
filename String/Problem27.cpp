ecf7508ff7527d5c7eb95631c66262cd24adb5ca
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 22:55:06 +0530

    [Create] String/Problem27.cpp

---
+++ String/Problem27.cpp
@@ -0,0 +1,22 @@
+/*
+    Reverse a string.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string str;
+        cin>>str;
+    }
+
+    return 0;
+}
