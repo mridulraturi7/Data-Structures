6b2754ac3b07906e8e2893fd3c8af34e47a6ace2
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 23:24:22 +0530

    [Done] String/Problem30.cpp

--- String/Problem30.cpp
+++ String/Problem30.cpp
@@ -4,17 +4,42 @@
 
 #include<iostream>
 #include<string>
+#include<sstream>
 
 using namespace std;
+
+class Solution
+{
+    public:
+        string firstAlphabet(string str)
+        {
+            string res, temp;
+
+            stringstream stream(str);
+
+            while(stream >> temp)
+            {
+                res.push_back(temp[0]);
+            }
+
+            return res;
+        }
+};
 
 int main()
 {
     int testCase;
     cin>>testCase;
 
+    cin.ignore();
+
     while(testCase-- != 0)
     {
+        string str;
+        getline(cin, str);
 
+        Solution ob;
+        cout<<ob.firstAlphabet(str)<<endl;
     }
 
     return 0;
