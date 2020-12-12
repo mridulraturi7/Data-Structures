bedff97bc58c05cb3b5e1b7db3478fd1dfc95f13
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 22:43:49 +0530

    [Create] String/Problem26.cpp

---
+++ String/Problem26.cpp
@@ -0,0 +1,20 @@
+/*
+    Concatenate and reverse the resultant string.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
