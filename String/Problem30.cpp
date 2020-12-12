7c9330697aba6a67e32f8941238a7135285e26f7
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-12 23:18:48 +0530

    [Create] String/Problem30.cpp

---
+++ String/Problem30.cpp
@@ -0,0 +1,21 @@
+/*
+    Print first letter of every word in the string.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
