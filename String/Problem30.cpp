a62841bde78618d16d3ca87847c3db996a36ff74
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-13 22:13:51 +0530

    [Modify] String/Problem30.cpp

--- String/Problem30.cpp
+++ String/Problem30.cpp
@@ -1,6 +1,6 @@
 /*
     Print first letter of every word in the string.
-    Problem is taken from GFG.
+    
     Problem Statement - https://practice.geeksforgeeks.org/problems/print-first-letter-of-every-word-in-the-string3632/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
     Difficulty - Basic
 */
