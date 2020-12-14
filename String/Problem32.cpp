ea2073b542517dd35c8fabe106ce830ca50ed00f
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-14 22:56:07 +0530

    [Done] String/Problem32.cpp

--- String/Problem32.cpp
+++ String/Problem32.cpp
@@ -14,7 +14,20 @@ int main()
 
     while(testCase-- != 0)
     {
+        string str;
+        cin>>str;
 
+        int count = 1;
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(isupper(str[i]))
+            {
+                count++;
+            }
+        }
+
+        cout<<count<<endl;
     }
 
     return 0;
