8389e442ece9f46f592f3a91d14756de000a1a74
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-14 22:50:09 +0530

    [Modify] String/Problem31.cpp

--- String/Problem31.cpp
+++ String/Problem31.cpp
@@ -10,6 +10,20 @@
 
 using namespace std;
 
+int countCorrespondingPositions(string s1, string s2)
+{
+    int count = 0;
+    for(int i = 0; i < s1.length(); i++)
+    {    
+        if(tolower(s1[i]) == tolower(s2[i]))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 int main()
 {
     int testCase;
@@ -20,16 +34,7 @@ int main()
         string s1, s2;
         cin>>s1>>s2;
 
-        int count = 0;
-        for(int i = 0; i < s1.length(); i++)
-        {
-            if(tolower(s1[i]) == tolower(s2[i]))
-            {
-                count++;
-            }
-        }
-
-        cout<<count<<endl;
+        cout<<countCorrespondingPositions(s1, s2)<<endl;
     }
 
     return 0;
