d6ba1bf65bc3019d95596544a9d36635321beb80
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-14 22:47:42 +0530

    [Done] String/Problem31.cpp

--- String/Problem31.cpp
+++ String/Problem31.cpp
@@ -3,6 +3,7 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -16,7 +17,16 @@ int main()
         string s1, s2;
         cin>>s1>>s2;
 
-        
+        int count = 0;
+        for(int i = 0; i < s1.length(); i++)
+        {
+            if(tolower(s1[i]) == tolower(s2[i]))
+            {
+                count++;
+            }
+        }
+
+        cout<<count<<endl;
     }
 
     return 0;
