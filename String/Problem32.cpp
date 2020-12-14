da123c8b503a05d27e28185b08c56c0d46a2a459
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-14 22:58:45 +0530

    [Modify] String/Problem32.cpp

--- String/Problem32.cpp
+++ String/Problem32.cpp
@@ -1,5 +1,8 @@
 /*
     The Counting Game.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/the-counting-game/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1#
+    Difficulty - Basic
 */
 
 #include<iostream>
