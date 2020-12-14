0894acc09665b8262cfab3ae45c94332dabc881e
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-14 22:44:52 +0530

    [Create] String/Problem31.cpp

---
+++ String/Problem31.cpp
@@ -0,0 +1,23 @@
+/*
+    C++ Corresponding Position in the two strings that hold exactly the same characters.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string s1, s2;
+        cin>>s1>>s2;
+
+        
+    }
+
+    return 0;
+}
