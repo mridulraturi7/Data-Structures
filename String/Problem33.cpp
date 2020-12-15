3c2170a689d93766b856bc2efd28ce39d2f2aa7a
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-15 21:16:17 +0530

    [Create] String/Problem33.cpp

---
+++ String/Problem33.cpp
@@ -0,0 +1,20 @@
+/*
+    Rock Paper Scissors.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
