556d42987e4e9d7bf378016bee4f9a1d7fbc3747
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-15 21:31:44 +0530

    [Done] String/Problem33.cpp

--- String/Problem33.cpp
+++ String/Problem33.cpp
@@ -3,8 +3,32 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
+
+string findTheWinner(string str)
+{
+    //If there is rock and scissors
+    if((str[0] == 'R' && str[1] == 'S') || (str[0] == 'S' && str[1] == 'R'))
+    {
+        return str[0] == 'R' ? "A" : "B";
+    }
+
+    //If there is rock and paper
+    if((str[0] == 'R' && str[1] == 'P') || (str[0] == 'P' && str[1] == 'R'))
+    {
+        return str[0] == 'P' ? "A" : "B";
+    }
+
+    //If there is scissors and paper
+    if((str[0] == 'S' && str[1] == 'P') || (str[0] == 'P' && str[1] == 'S'))
+    {
+        return str[0] == 'S' ? "A" : "B";
+    }
+
+    return "DRAW";
+}
 
 int main()
 {
@@ -13,7 +37,10 @@ int main()
 
     while(testCase-- != 0)
     {
+        string str;
+        cin>>str;
 
+        cout<<findTheWinner(str)<<endl;
     }
 
     return 0;
