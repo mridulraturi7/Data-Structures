e1036454b3793d106548dd07cce04fc8a488372a
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-15 21:33:05 +0530

    [Modify] String/Problem33.cpp

--- String/Problem33.cpp
+++ String/Problem33.cpp
@@ -1,5 +1,8 @@
 /*
-    
+    Rock Paper Scissors.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/rock-paper-scissors/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
+    Difficulty - Basic
 */
 
 #include<iostream>
