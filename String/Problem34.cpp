48e2e9a2da337204db525224767d62840063f25f
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-15 22:24:34 +0530

    [Modify] String/Problem34.cpp

--- String/Problem34.cpp
+++ String/Problem34.cpp
@@ -1,8 +1,5 @@
 /*
-    Confused Pappu.
-    Problem is taken from GFG.
-    Problem Statement - https://practice.geeksforgeeks.org/problems/confused-pappu/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
-    Difficulty - Basic
+    
 */
 
 #include<iostream>
