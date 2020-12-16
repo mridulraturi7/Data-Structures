b2e2728162901a95f7165f7368649b8749f129bc
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-16 23:23:31 +0530

    [Modify] String/Problem35.cpp

--- String/Problem35.cpp
+++ String/Problem35.cpp
@@ -1,5 +1,8 @@
 /*
-    
+    The Penalty Shootout.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/the-penalty-shootout3810/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
+    Difficulty - Basic
 */
 
 #include<iostream>
