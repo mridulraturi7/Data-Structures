c5fec59803ba9fa9fe6ed87ff40f5893a2d2ef83
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-16 23:02:14 +0530

    [Done] String/Problem35.cpp

--- String/Problem35.cpp
+++ String/Problem35.cpp
@@ -7,6 +7,27 @@
 
 using namespace std;
 
+class Solution
+{
+    public:
+        int penaltyScore(string str)
+        {
+            int goal = 0;
+            for(int i = 0; i < str.length() - 1; i++)
+            {
+                if(str[i] == '2')
+                {
+                    if(str[i + 1] == '1')
+                    {
+                        goal++;
+                    }
+                }
+            }
+
+            return goal;
+        }
+};
+
 int main()
 {
     int testCase;
@@ -14,7 +35,11 @@ int main()
 
     while(testCase-- != 0)
     {
+        string str;
+        cin>>str;
 
+        Solution ob;
+        cout<<ob.penaltyScore(str)<<endl;
     }
 
     return 0;
