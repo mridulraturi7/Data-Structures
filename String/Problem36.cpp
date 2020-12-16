65b385dc0a687d5a1466384a2b168e31db36d8dd
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-16 23:16:40 +0530

    [Create] String/Problem36.cpp

---
+++ String/Problem36.cpp
@@ -0,0 +1,21 @@
+/*
+    Check if a string is Isogram or not.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        string str;
+    }
+
+    return 0;
+}
