a587bc644f186062bcadf287c8d186f125d5e0cf
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-17 17:48:43 +0530

    [Done] String/Problem38.cpp

--- String/Problem38.cpp
+++ String/Problem38.cpp
@@ -9,7 +9,20 @@ using namespace std;
 
 int countEvenSubstrings(string str)
 {
+    int count = 0;
+    for(int i = 0; i < str.length(); i++)
+    {
+        for(int len = 1; len <= str.length() - i; len++)
+        {
+            int num = stoi(str.substr(i, len));
+            if((num % 2) == 0)
+            {
+                count++;
+            }
+        }
+    }
 
+    return count;
 }
 
 int main()
