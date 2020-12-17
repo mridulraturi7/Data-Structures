e191acb9d6174620261de0de11d566b0037f6efd
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-17 17:42:36 +0530

    [Create] String/Problem38.cpp

---
+++ String/Problem38.cpp
@@ -0,0 +1,20 @@
+/*
+    Count Even Substrings.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        
+    }
+
+    return 0;
+}
