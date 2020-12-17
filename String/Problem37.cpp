03dd1ecf2ee730e4e3045e1413e425f49eee8a57
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-17 17:40:15 +0530

    [Modify] String/Problem37.cpp

--- String/Problem37.cpp
+++ String/Problem37.cpp
@@ -1,5 +1,8 @@
 /*
     Maximum Occurring Character.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/maximum-occuring-character-1587115620/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1#
+    Difficulty - Basic
 */
 
 #include<iostream>
