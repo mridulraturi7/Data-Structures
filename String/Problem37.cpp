1ac6c1935d07bb927a804b269852e9dd9d5eedab
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-17 17:27:54 +0530

    [Create] String/Problem37.cpp

---
+++ String/Problem37.cpp
@@ -0,0 +1,20 @@
+/*
+    Maximum Occurring Character.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
