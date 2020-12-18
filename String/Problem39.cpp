79ff6058318f8745dca7d3a6c41a736a9ca86861
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-18 23:02:43 +0530

    [Modify] String/Problem39.cpp

--- String/Problem39.cpp
+++ String/Problem39.cpp
@@ -1,9 +1,4 @@
-/*
-    Sort String.
-    Problem is taken from GFG.
-    Problem Statement - https://practice.geeksforgeeks.org/problems/sort-string/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
-    Difficulty - Basic
-*/
+
 
 #include<iostream>
 #include<string>
