d7b882b878a61becfb6f0e37a866db75bcd5daf9
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-18 23:00:48 +0530

    [Done] String/Problem40.cpp

--- String/Problem40.cpp
+++ String/Problem40.cpp
@@ -7,6 +7,21 @@
 
 using namespace std;
 
+long binarySubstring(int n, string a)
+{
+    int count = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] == '1')
+        {
+            count++;
+        }
+    }
+
+    return (count * (count - 1)) / 2;
+}
+
 int main()
 {
     int testCase;
@@ -14,7 +29,13 @@ int main()
 
     while(testCase-- != 0)
     {
-        
+        int n;
+        cin>>n;
+
+        string str;
+        cin>>str;
+
+        cout<<binarySubstring(n, str)<<endl;   
     }
 
     return 0;
