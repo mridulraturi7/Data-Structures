3d3a71b1b6dcbe14c4cc248152b9b7e8b9bcc0e5
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-18 23:02:32 +0530

    [Modify] String/Problem40.cpp

--- String/Problem40.cpp
+++ String/Problem40.cpp
@@ -1,5 +1,8 @@
 /*
     Binary String.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/binary-string-1587115620/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1#
+    Difficulty - Basic
 */
 
 #include<iostream>
