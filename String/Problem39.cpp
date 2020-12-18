624cc9f53dbf428cad815277dfd246a9d4ffe20d
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-18 22:43:22 +0530

    [Done] String/Problem39.cpp

--- String/Problem39.cpp
+++ String/Problem39.cpp
@@ -4,8 +4,26 @@
 
 #include<iostream>
 #include<string>
+#include<map>
 
 using namespace std;
+
+string sortString(string str)
+{
+    map<char, int> hash;
+    for(int i = 0; i < str.length(); i++)
+    {
+        hash[str[i]]++;
+    }
+
+    string res;
+    for(auto i : hash)
+    {
+        res.push_back(i.first);
+    }
+
+    return res;
+}
 
 int main()
 {
