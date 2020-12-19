07def06002a5e6eb6481ae34f3d62e54654aca48
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-19 22:48:52 +0530

    [Modify] String/Problem40.cpp

--- String/Problem40.cpp
+++ String/Problem40.cpp
@@ -32,7 +32,16 @@ int main()
     int testCase;
     cin>>testCase;
 
-    
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        string str;
+        cin>>str;
+
+        cout<<binarySubstring(n, str)<<endl;   
+    }
 
     return 0;
 }
