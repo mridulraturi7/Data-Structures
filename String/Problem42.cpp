2aea66d42157de07ed2540cf963d9041b969d321
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-20 22:36:44 +0530

    [Create] String/Problem42.cpp

---
+++ String/Problem42.cpp
@@ -0,0 +1,21 @@
+/*
+    Sort a String.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
