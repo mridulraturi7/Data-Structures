2568393a21a8b64bb9f94ecee3747e561fb25129
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-20 22:28:36 +0530

    [Modify] String/Problem41.cpp

--- String/Problem41.cpp
+++ String/Problem41.cpp
@@ -3,12 +3,27 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+int calculateSum(string str)
+{
+    
+}
+
 int main()
 {
+    int testCase;
+    cin>>testCase;
 
+    while(testCase-- != 0)
+    {
+        string str;
+        cin>>str;
+
+        cout<<calculateSum(str)<<endl;
+    }
 
     return 0;
 }
