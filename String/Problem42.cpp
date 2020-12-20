ae6235de96ae8d722c65dfdbed95cb28afb46fb9
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-20 22:44:40 +0530

    [Modify] String/Problem42.cpp

--- String/Problem42.cpp
+++ String/Problem42.cpp
@@ -1,5 +1,8 @@
 /*
     Sort a String.
+    Problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/sort-a-string/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1#
+    Difficulty - Basic
 */
 
 #include<iostream>
@@ -39,7 +42,7 @@ int main()
     {
         int n;
         cin>>n;
-        
+
         string str;
         cin>>str;
 
