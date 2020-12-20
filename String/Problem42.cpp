a8b58f3f513249a46a3c541c4bcda94ae41210cd
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-20 22:41:08 +0530

    [Modify] String/Problem42.cpp

--- String/Problem42.cpp
+++ String/Problem42.cpp
@@ -4,12 +4,30 @@
 
 #include<iostream>
 #include<string>
+#include<map>
 
 using namespace std;
 
 string sortString(string str)
 {
-    
+    map<char, int> hash;
+    for(int i = 0; i < str.length(); i++)
+    {
+        hash[str[i]]++;
+    }
+
+    string res;
+
+    for(auto i : hash)
+    {
+        while(i.second != 0)
+        {
+            res.push_back(i.first);
+            i.second--;
+        }
+    }
+
+    return res;
 }
 
 int main()
