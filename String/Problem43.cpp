27dee996b9b1c2fdad3806f6952d94e6eff245ed
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-20 22:50:57 +0530

    [Create] String/Problem43.cpp

---
+++ String/Problem43.cpp
@@ -0,0 +1,21 @@
+/*
+    Remove character.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+
+    }
+
+    return 0;
+}
