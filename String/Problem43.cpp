32a7ab63063544eeb93b45355a55076084afa1c1
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-22 22:16:41 +0530

    [Modify] String/Problem43.cpp

--- String/Problem43.cpp
+++ String/Problem43.cpp
@@ -1,8 +1,5 @@
 /*
-    Remove character.
-    Problem is taken from GFG.
-    Problem Statement - https://practice.geeksforgeeks.org/problems/remove-character/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
-    Difficulty - Basic
+    
 */
 
 #include<iostream>
