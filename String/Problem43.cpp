73d0bfa0e153f755c548dfbb453a7ee7b6ce90bb
Mridul Raturi <64581987+mridulraturi7@users.noreply.github.com>
2021-04-29 01:29:01 +0530

    [Update] String/Problem43.cpp

--- String/Problem43.cpp
+++ String/Problem43.cpp
@@ -3,7 +3,7 @@
     Problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/remove-character/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
     Difficulty - Basic
-*/
+*
 
 #include<iostream>
 #include<string>
