53aaf17fb279f67d59074b87060c501327ca1e95
Mridul Raturi <64581987+mridulraturi7@users.noreply.github.com>
2021-05-02 23:46:21 +0530

    [Modify] String/Problem41.cpp

--- String/Problem41.cpp
+++ String/Problem41.cpp
@@ -1,9 +1,4 @@
-/*
-    Sum of numbers in string.
-    Problem is taken from GFG.
-    Problem Statement - https://practice.geeksforgeeks.org/problems/sum-of-numbers-in-string/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1
-    Difficulty - Basic
-*/
+
 
 #include<iostream>
 #include<string>
