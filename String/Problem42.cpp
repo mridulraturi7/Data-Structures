2aeddb36eb1e71a2d2f32f192c5e55283cffc128
Mridul Raturi <64581987+mridulraturi7@users.noreply.github.com>
2021-06-09 20:59:00 +0530

    [Update] String/Problem42.cpp

--- String/Problem42.cpp
+++ String/Problem42.cpp
@@ -1,9 +1,4 @@
-/*
-    Sort a String.
-    Problem is taken from GFG.
-    Problem Statement - https://practice.geeksforgeeks.org/problems/sort-a-string/0/?category[]=Strings&difficulty[]=-1&page=1&query=category[]Stringsdifficulty[]-1page1#
-    Difficulty - Basic
-*/
+
 
 #include<iostream>
 #include<string>
